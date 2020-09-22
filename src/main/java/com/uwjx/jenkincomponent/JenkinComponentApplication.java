5c554ad2b4516e7d34df6a68ad246fd340efe91f
Hugh Wang <18501667737@163.com>
2020-09-22 22:15:50 +0800

    By Huan

---
+++ src/main/java/com/uwjx/jenkincomponent/JenkinComponentApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.jenkincomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinComponentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/jenkincomponent/JenkinComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.jenkincomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
